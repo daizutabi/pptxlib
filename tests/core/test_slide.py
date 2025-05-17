4b778ed1c4fdfa4cd66540a7a489ba23112fff57
Daizu <daizutabi@gmail.com>
2025-05-17 14:28:35 +0900

    Enhance Presentation class with size method and property setters
    - Added width and height property setters to the Presentation class for dynamic resizing.
    - Introduced a size method to set both width and height in one call.
    - Updated tests to reflect changes in presentation size handling and ensure correct functionality.

--- tests/core/test_slide.py
+++ tests/core/test_slide.py
@@ -5,6 +5,7 @@ import pytest
 from win32com.client import constants
 
 from pptxlib.core.app import is_app_available
+from pptxlib.core.presentation import Presentations
 from pptxlib.core.slide import Slide, Slides
 
 pytestmark = pytest.mark.skipif(
@@ -42,10 +43,11 @@ def test_layout(slides: Slides):
     assert slide.api.Layout == constants.ppLayoutBlank
 
 
-def test_png(slide: Slide, tmp_path: Path):
+def test_png(prs: Presentations, tmp_path: Path):
+    slide = prs.add().size(600, 300).slides.add()
     data = slide.png()
     assert data.startswith(b"\x89PNG")
     path = tmp_path.joinpath("a.png")
     path.write_bytes(data)
     image = PIL.Image.open(path)
-    assert image.size == (1280, 720)
+    assert image.size == (600 * 4 / 3, 300 * 4 / 3)
--- tests/shape.py
+++ tests/shape.py
@@ -8,12 +8,13 @@ from pptxlib.core.app import App
 def main():
     app = App()
     app.presentations.close()
-    pr = app.presentations.add()
+    pr = app.presentations.add().size(400, 300)
+
     slide = pr.slides.add()
     shapes = slide.shapes
-    s1 = shapes.add("Rectangle", 100, 100, 100, 100)
-    s2 = shapes.add("Oval", 150, 150, 90, 80)
-    slide.export(Path(__file__).parent / "a.svg")
+    shapes.add("Rectangle", 100, 100, 100, 100)
+    shapes.add("Oval", 150, 150, 90, 80)
+    slide.export(Path(__file__).parent / "a.png")
 
 
 if __name__ == "__main__":
